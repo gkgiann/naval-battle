f92ff09f965f31c3026aa9f4854537a9aa439d33
gian <gian@pop-os.localdomain>
2023-09-29 14:24:55 -0300

    game scene

---
+++ scenes/game.lua
@@ -0,0 +1,13 @@
+Game = Class:extend()
+
+function Game:new()
+end
+
+function Game:update(dt)
+end
+
+function Game:draw()
+    love.window.setTitle("Batalha Naval")
+    love.graphics.print("Com os navios posicionados, hora de batalhar")
+end
+    
